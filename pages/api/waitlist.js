1371fe80532d976aad11f05b8aefa3e2f10e3dee
Joshua-Zou <joshyzou@gmail.com>
2021-08-04 16:45:23 -0700

    things

--- pages/api/waitlist.js
+++ pages/api/waitlist.js
@@ -1,6 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
   //
-  return res.send("Success!")
+  if (!req.body) return res.send("STAP IT PLESSSSSS");
+  if (!req.body.email || !req.body.name || !req.body.level) return res.send("STAP IT PLESSSSSS");
+  const fs = require("file-system");
+  let secrets = process.env.secrets;
+  if (!secrets){
+    secrets = fs.readFileSync("../../secrets.json");
+  };
+  secrets = JSON.parse(secrets);
+  const uri = secrets.mongodb;
+  const {MongoClient} = require('mongodb')
+  const mongoclient = new MongoClient(uri, {poolSize: 10, bufferMaxEntries: 0, useNewUrlParser: true,useUnifiedTopology: true});
+  await mongoclient.connect();
+  // do things
+  await mongoclient.close();
+  return res.send(`Success! EMAIL: ${req.body.email}, NAME: ${req.body.name}, LEVEL: ${req.body.level}`);
 }
