cd11d6760d172e7931e235373c6a148da67c32c4
Joshua-Zou <joshyzou@gmail.com>
2021-08-04 16:30:24 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Vivid hacks Backend</title>
+      </Head>
+
+      <main>
+        Hey! We have absolutely no idea how you got here, but you probably *don't* want to be here, so here's a couple of places that you *could* be.
+      </main>
+    </div>
+  )
+}
