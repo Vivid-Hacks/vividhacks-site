d7a5a50b6b0b6313428088965324d35bbd08f441
warren yun <xxwarrenhdxx@gmail.com>
2021-08-04 22:14:30 -0400

    Footer template

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import footerStyles from '../styles/Footer.module.css';
+
+export default function Footer() {
+    return (
+        <div className={footerStyles.container}>
+        
+        </div>
+    );
+}
