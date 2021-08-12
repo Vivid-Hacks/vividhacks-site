a276bf760c09b7383bf5441a020155b0b440a171
Joshua-Zou <joshyzou@gmail.com>
2021-08-12 16:37:21 -0700

    added more to register now section

--- components/register/index.js
+++ components/register/index.js
@@ -1,10 +1,20 @@
 import styles from "./register.module.css";
 
 import Link from "next/link";
+
+function showModal() {
+
+}
+
 const Register = () => {
-  return (
-    <button className={styles.button}>Register Now</button>
-  );
+    return (
+        <div>
+            <button onClick={showModal} className={styles.offset}><span className={styles.buttonSpan}>Register Now</span></button>
+            <div className={styles.invisible, styles.modal}>
+                asdf
+            </div>
+        </div>
+    );
 };
 
 export default Register;
