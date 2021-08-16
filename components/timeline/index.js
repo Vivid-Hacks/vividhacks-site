2a52dabddcc7193203524c49a2d67a477085c21c
Joshua-Zou <joshyzou@gmail.com>
2021-08-16 16:02:15 -0700

    added timeline

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -1,7 +1,11 @@
 import styles from "./timeline.module.css";
-
+import Iframe from "react-iframe"
 const Timeline = () => {
-  return <h2 id="timeline" className={styles.heading}>Timeline (TBA)</h2>;
+  return (
+  <div>
+    <Iframe className={styles.iframe} url="https://time.graphics/embed?v=1&id=537355"/>
+  </div>
+    );
 };
 
 export default Timeline;
