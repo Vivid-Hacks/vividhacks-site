2fc513ff47a4b8dd68e20709cf24bb1857b1957d
Joshua-Zou <joshyzou@gmail.com>
2021-08-22 14:50:45 -0700

    made animations better

--- components/Sponsors/index.js
+++ components/Sponsors/index.js
@@ -1,15 +1,38 @@
 import styles from "./sponser.module.css";
+import * as useOnScreen from "../Hooks/isVisible";
+import React, { useRef, useState } from "react"
+class Sponsor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.containerRef = React.createRef();
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  handleScroll() {
+    const element = this.containerRef.current;
+    const windowOffsetTop = window.innerHeight + window.scrollY;
 
-const Sponsor = () => {
-  return (
-    <div className={styles.animate}>
-    <div className={styles.container}>
-      <h2 className={styles.heading}>Sponsors</h2>
-      <p className={styles.para}>Want to be our partner?</p>
-      <button className={styles.button}>LEARN MORE</button>
-    </div>
-    </div>
-  );
+    const elementBoxOffsetTop = element.offsetTop;
+    if (windowOffsetTop >= elementBoxOffsetTop) {
+      this.containerRef.current.classList.add("fade");
+      console.log("added!");
+    } else {
+      this.containerRef.current.classList.remove("fade")
+    }
+  }
+  render() {
+    if (typeof window === 'object') {
+      document.addEventListener("scroll", this.handleScroll, true);
+    }
+    return (
+      <div ref={this.containerRef}>
+        <div className={styles.container}>
+          <h2 className={styles.heading}>Sponsors</h2>
+          <p className={styles.para}>Want to be our partner?</p>
+          <button className={styles.button}>LEARN MORE</button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Sponsor;
--- components/about/index.js
+++ components/about/index.js
@@ -4,8 +4,6 @@ import Timeline from "../timeline/";
 import * as React from "react";
 import { render } from "react-dom";
 
-
-
 class About extends React.Component {
   constructor(props) {
     super(props);
@@ -13,26 +11,38 @@ class About extends React.Component {
     this.textInput = React.createRef();
     this.addEvLisner = this.addEvLisner.bind(this);
     this.handleScroll = this.handleScroll.bind(this);
-    this.growingLine = {width: 0};
+    this.growingLine = { width: 0 };
     this.lastScrollTop = 0;
+    this.containerRef = React.createRef();
   }
-  handleScroll(){
-      var st = window.scrollY;
-      if (st > this.lastScrollTop) {
-        let newWidth = Number(this.growingLine.width.toString().slice(0, this.growingLine.width.toString().length-2))+0.4;
-        this.growingLine = {
-          width : newWidth+"vw"
-        }
-        this.forceUpdate();
-      } else {
-        let newWidth = Number(this.growingLine.width.toString().slice(0, this.growingLine.width.toString().length-2))-0.4;
-        if (newWidth < 0) newWidth = 0
-        this.growingLine = {
-          width : newWidth+"vw"
-        }
-        this.forceUpdate()
+  handleScroll() {
+
+    const element = this.containerRef.current;
+    const windowOffsetTop = window.innerHeight + window.scrollY;
+
+    const elementBoxOffsetTop = element.offsetTop;
+    if (windowOffsetTop >= elementBoxOffsetTop) {
+      this.containerRef.current.classList.add("fade")
+    }else{
+      this.containerRef.current.classList.remove("fade")
+    }
+
+    var st = window.scrollY;
+    if (st > this.lastScrollTop) {
+      let newWidth = Number(this.growingLine.width.toString().slice(0, this.growingLine.width.toString().length - 2)) + 0.4;
+      this.growingLine = {
+        width: newWidth + "vw"
       }
-      this.lastScrollTop = st;
+      this.forceUpdate();
+    } else {
+      let newWidth = Number(this.growingLine.width.toString().slice(0, this.growingLine.width.toString().length - 2)) - 0.4;
+      if (newWidth < 0) newWidth = 0
+      this.growingLine = {
+        width: newWidth + "vw"
+      }
+      this.forceUpdate()
+    }
+    this.lastScrollTop = st;
   }
 
   addEvLisner() {
@@ -43,33 +53,32 @@ class About extends React.Component {
   }
   render() {
     if (typeof window === 'object') {
-      document.addEventListener("scroll",this.handleScroll, true);
-
+      document.addEventListener("scroll", this.handleScroll, true);
     }
     return (
-      <div className={styles.animate}>
-      <div id="about" className={styles.body} onClick={this.handleScroll}>
-        <div className={styles.division}>
-          <hr className={styles.growingLine} style={this.growingLine}></hr>
-        </div>
-        <div className={styles.container}>
-          <span className={styles.bigtext}>What is VividHacks?</span>
-          <div className={styles.mainContent}>
-            <div className={styles.desc}>
-              Vivid Hacks is a 60 hour hackathon for everybody.
-              We'll provide you with the mentors to bring your dream project to <span style={{ color: "#68EB3B" }}>reality</span>.
-              Build something <span style={{ color: "#EDC229" }}>crazy</span>.
-              Something <span style={{ color: "#E36C6C" }}>game-changing</span>.
-              Something <span style={{ color: "#2DC2CC" }}>cutting edge</span>.
-              Vivid Hacks is a weekend experience you won't want to miss. 😛
-              <div className={styles.faqwrapper}>
-                <Timeline />
+      <div ref={this.containerRef}>
+        <div id="about" className={styles.body} onClick={this.handleScroll}>
+          <div className={styles.division}>
+            <hr className={styles.growingLine} style={this.growingLine}></hr>
+          </div>
+          <div className={styles.container}>
+            <span className={styles.bigtext}>What is VividHacks?</span>
+            <div className={styles.mainContent}>
+              <div className={styles.desc}>
+                Vivid Hacks is a 60 hour hackathon for everybody.
+                We'll provide you with the mentors to bring your dream project to <span style={{ color: "#68EB3B" }}>reality</span>.
+                Build something <span style={{ color: "#EDC229" }}>crazy</span>.
+                Something <span style={{ color: "#E36C6C" }}>game-changing</span>.
+                Something <span style={{ color: "#2DC2CC" }}>cutting edge</span>.
+                Vivid Hacks is a weekend experience you won't want to miss. 😛
+                <div className={styles.faqwrapper}>
+                  <Timeline />
+                </div>
               </div>
+              <iframe src="https://www.youtube.com/embed/KUIigF_7P5c" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
-            <iframe src="https://www.youtube.com/embed/KUIigF_7P5c" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
         </div>
-      </div>
       </div>
     );
   }
