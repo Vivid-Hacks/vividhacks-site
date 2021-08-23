96c408678daaaa4dc3571d0801adad873d044bc9
Joshua-Zou <joshyzou@gmail.com>
2021-08-23 10:16:57 -0700

    fixed building bug

--- pages/_app.js
+++ pages/_app.js
@@ -7,23 +7,24 @@ import App, { Container } from 'next/app';
 import NProgress from 'nprogress';
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
+  if (publicRuntimeConfig !== undefined){
+    NProgress.configure({ showSpinner: publicRuntimeConfig.NProgressShowSpinner });
 
-NProgress.configure({ showSpinner: publicRuntimeConfig.NProgressShowSpinner });
 
-
-Router.onRouteChangeStart = (url) => {
-    // Some page has started loading
-    NProgress.start();
-};
-
-Router.onRouteChangeComplete = (url) => {
-    // Some page has finished loading
-    NProgress.done();
-};
-
-Router.onRouteChangeError = (err, url) => {
-  NProgress.done();
-};
+    Router.onRouteChangeStart = (url) => {
+        // Some page has started loading
+        NProgress.start();
+    };
+    
+    Router.onRouteChangeComplete = (url) => {
+        // Some page has finished loading
+        NProgress.done();
+    };
+    
+    Router.onRouteChangeError = (err, url) => {
+      NProgress.done();
+    };
+  }
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
