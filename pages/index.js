7af1eab07b76b96fd3a1c0f57b427c913c50919f
Joshua-Zou <joshyzou@gmail.com>
2021-08-30 16:09:41 -0700

    things

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,33 @@ import Faqs from "../components/faqs"
 import Footer from "../components/Footer";
 import * as React from "react";
 import { render } from "react-dom";
+import MetaTags from 'react-meta-tags';
 
 
 const index = () => {
 
 
-    return (
-      <div>
-        <Navbar />
-        <Landing />
-        <About />
-        <Sponsors />
-        <Team />
-        <TeamGrid />
-        <Faqs />
-        <Footer />
-      </div >
-    );
+  return (
+    <div>
+      <MetaTags>
+        <title>Vivid Hacks</title>
+        <meta name="description" content="Some description."></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content="Vivid Hacks" ></meta>
+        <meta property="og:description" content="Vivid Hacks is 65 hour hackathon for everyone"></meta>
+        <meta property="og:url" content="https://vividhacks.tech"></meta>
+        <meta property="og:image" content="https://www.vividhacks.tech/logo.svg"></meta>
+      </MetaTags>
+      <Navbar />
+      <Landing />
+      <About />
+      <Sponsors />
+      <Team />
+      <TeamGrid />
+      <Faqs />
+      <Footer />
+    </div >
+  );
 }
 
 export default index
