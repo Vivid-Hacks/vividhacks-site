b32f2bf019cbd86ab313941ea4aeef35fe607468
Joshua-Zou <joshyzou@gmail.com>
2021-09-02 15:32:07 -0700

    added more sponsors

--- components/Sponsors/index.js
+++ components/Sponsors/index.js
@@ -9,13 +9,13 @@ class Sponsor extends React.Component {
   }
   handleScroll() {
     const element = this.containerRef.current;
-    if (element){
+    if (element) {
       const windowOffsetTop = window.innerHeight + window.scrollY;
-      
+
       const elementBoxOffsetTop = element.offsetTop;
       if (windowOffsetTop >= elementBoxOffsetTop) {
         this.containerRef.current.classList.add("fade")
-      }else{
+      } else {
         this.containerRef.current.classList.remove("fade")
       }
     }
@@ -28,6 +28,16 @@ class Sponsor extends React.Component {
       <div ref={this.containerRef}>
         <div className={styles.container}>
           <h2 className={styles.heading}>Sponsors</h2>
+          <div className={styles.sponsorGrid}>
+            <div className={styles.sponsor}>
+              <a href="https://www.echoar.xyz/" target="_blank"><img src="/echoarlogo.png"></img></a>
+              <div><span>Echo AR</span></div>
+            </div>
+            <div className={styles.sponsor}>
+              <a href="https://hackclub.org" target="_blank"><img src="/hackclub.png" width="215"></img></a>
+              <div><span>Hack Club</span></div>
+            </div>
+          </div>
           <p className={styles.para}>Want to be our partner?</p>
           <button className={styles.button}>LEARN MORE</button>
         </div>
