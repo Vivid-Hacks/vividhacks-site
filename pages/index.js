db249b46abe68c32310682c1e5e3bc83fc9fe3a4
Manish674 <manishchoudhary4905@gmail.com>
2021-09-03 19:09:08 +0530

    nothing the code just auto formated

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,29 @@ import Timeline from "../components/Timeline";
 import Sponsors from "../components/Sponsors";
 import Team from "../components/Team";
 import TeamGrid from "../components/TeamGrid";
-import Faqs from "../components/faqs"
+import Faqs from "../components/faqs";
 import Footer from "../components/Footer";
 import * as React from "react";
 import { render } from "react-dom";
-import MetaTags from 'react-meta-tags';
-
+import MetaTags from "react-meta-tags";
 
 const index = () => {
-
-
   return (
     <div>
       <MetaTags>
         <title>Vivid Hacks</title>
         <meta name="description" content="Some description."></meta>
         <meta property="og:type" content="website"></meta>
-        <meta property="og:title" content="Vivid Hacks" ></meta>
-        <meta property="og:description" content="Vivid Hacks is 65 hour hackathon for everyone"></meta>
+        <meta property="og:title" content="Vivid Hacks"></meta>
+        <meta
+          property="og:description"
+          content="Vivid Hacks is 65 hour hackathon for everyone"
+        ></meta>
         <meta property="og:url" content="https://vividhacks.tech"></meta>
-        <meta property="og:image" content="https://www.vividhacks.tech/logo.svg"></meta>
+        <meta
+          property="og:image"
+          content="https://www.vividhacks.tech/logo.svg"
+        ></meta>
       </MetaTags>
       <Navbar />
       <Landing />
@@ -34,8 +37,8 @@ const index = () => {
       <TeamGrid />
       <Faqs />
       <Footer />
-    </div >
+    </div>
   );
-}
+};
 
-export default index
+export default index;
