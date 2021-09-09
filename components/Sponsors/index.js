46c9d5b38efa5d5339423481fa727c3714db4743
Joshua-Zou <joshyzou@gmail.com>
2021-09-09 16:31:32 -0700

    added donation iframe

--- components/Sponsors/index.js
+++ components/Sponsors/index.js
@@ -5,7 +5,12 @@ class Sponsor extends React.Component {
   constructor(props) {
     super(props);
     this.containerRef = React.createRef();
+    this.modalRef = React.createRef();
+    this.modalBtn = React.createRef();
+    this.coverBG = React.createRef();
+
     this.handleScroll = this.handleScroll.bind(this);
+    this.openModal = this.openModal.bind(this);
   }
   handleScroll() {
     const element = this.containerRef.current;
@@ -20,12 +25,29 @@ class Sponsor extends React.Component {
       }
     }
   }
+  openModal(){
+    this.modalRef.current.style.display = "block";
+    this.coverBG.current.style.display = "block";
+  }
   render() {
+    var classThis = this;
     if (typeof window === 'object') {
       document.addEventListener("scroll", this.handleScroll, true);
+      document.addEventListener("click", function(event){
+        if (classThis.modalRef.current.contains(event.target)){
+        }else{
+          if (classThis.modalBtn.current.contains(event.target)) return;
+          classThis.modalRef.current.style.display = "none"
+          classThis.coverBG.current.style.display = "none"
+        }
+      })
     }
     return (
       <div ref={this.containerRef}>
+        <div className={styles.modal} ref={this.modalRef} style={{display: "none"}}>
+          <iframe src="https://bank.hackclub.com/donations/start/vivid-hacks"></iframe>
+        </div>
+        <div className={styles.cover} ref={this.coverBG} style={{display: "none"}}></div>
         <div className={styles.container}>
           <h2 className={styles.heading}>Sponsors</h2>
           <div className={styles.sponsorGrid}>
@@ -39,7 +61,9 @@ class Sponsor extends React.Component {
             </div>
           </div>
           <p className={styles.para}>Want to be our partner?</p>
-          <button className={styles.button}><a href="mailto:vividhacks@gmail.com">LEARN MORE</a></button>
+          <button className={styles.button} style={{marginBottom: 0}}><a href="mailto:vividhacks@gmail.com">LEARN MORE</a></button>
+          <p className={styles.para}>Or </p>
+          <button className={styles.button} ref={this.modalBtn}><a onClick={this.openModal} style={{cursor: "pointer"}}>Donate to us!</a></button>
         </div>
       </div>
     );
