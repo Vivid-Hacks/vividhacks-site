0da4d1a29dd891da6dc7e939bd57ffef5e01f01c
Joshua-Zou <joshyzou@gmail.com>
2021-10-10 10:19:08 -0700

    created discord modal

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -13,7 +13,7 @@ var lineRight = styles.lineRight;
 var lineMiddle = styles.lineMiddle;
 
 class Navbar extends React.Component {
-  
+
   constructor(props) {
     super(props);
     // create a ref to store the textInput DOM element
@@ -22,7 +22,10 @@ class Navbar extends React.Component {
     this.menu = styles.linkContainer;
     this.menuStyling = {};
     this.navbarRef = React.createRef();
-    this.handleScroll = this.handleScroll.bind(this)
+    this.handleScroll = this.handleScroll.bind(this);
+    this.discordModal = React.createRef();
+    this.bg = React.createRef();
+    this.callback = this.callback.bind(this)
   }
 
   addEvLisner() {
@@ -31,30 +34,37 @@ class Navbar extends React.Component {
   }
   componentDidMount() {
     var oldThis = this;
-    if (window.pageYOffset === 0){
+    if (window.pageYOffset === 0) {
       this.navbarRef.current.style.background = "rgba(0, 0, 0, 0)"
     }
-
-
     window.addEventListener('scroll', this.handleScroll);
+  }
+  callback() {
+    this.discordModal.current.style.display = "block"; this.bg.current.style.display = "inherit";
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
   handleScroll() {
     if (!this.navbarRef.current) return;
-    if (window.pageYOffset === 0){
+    if (window.pageYOffset === 0) {
       this.navbarRef.current.style.background = "rgba(0, 0, 0, 0)"
-    }else{
+    } else {
       this.navbarRef.current.style.background = ""
     }
   }
   render() {
-      return(
-        <div className={styles.container} ref={this.navbarRef}>
-          <Content/>
+    return (
+      <div className={styles.container} ref={this.navbarRef}>
+        <div className={styles.bg} ref={this.bg} style={{display: "none"}} onClick={() => { this.discordModal.current.style.display = "none"; this.bg.current.style.display = "none"; }}></div>
+        <div className={styles.discord} style={{display: "none"}} ref={this.discordModal} id="discordModal">
+          <iframe src="https://discord.com/widget?id=873307975572402207&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+          <br />
+          <button onClick={() => { window.open("https://discord.com/invite/yhHRz8ng") }} className={styles.joindiscord}><a href="#" target="_blank">Join Discord!</a></button>
         </div>
-      )
+        <Content callback={this.callback} />
+      </div>
+    )
   }
 }
 
@@ -86,7 +96,7 @@ class Content extends React.Component {
     }
     this.forceUpdate();
   }
-  componentDidMount(){
+  componentDidMount() {
     let elm = this;
     window.addEventListener("click", function (thing) {
       if (thing.target.tagName === "A" || thing.target.tagName === "HR") return;
@@ -97,169 +107,169 @@ class Content extends React.Component {
       elm.forceUpdate();
     });
   }
-  render(){
-    if (typeof window !== "object"){
+  render() {
+    if (typeof window !== "object") {
       return <div></div>
     }
     if (window.location.pathname === "/") {
       return (
         <div>
-            <div className={styles.logo}>
-              <CrossLink href="/">
-                <a>
-                  <h3 className={styles.logoText}>Vivid Hacks</h3>
-                </a>
-              </CrossLink>
-              <CrossLink href="/">
-                <a>
-                  <img
-                    src="logo.svg"
-                    alt="logo.svg"
-                    style={{ width: "5rem" }}
-                  />
-                </a>
-              </CrossLink>
-              <a
-                onClick={this.toggleMobileNavbar}
-                className={styles.menuIcon}
-              >
-                <hr className={lineLeft}></hr>
-                <hr className={lineMiddle}></hr>
-                <hr className={lineRight}></hr>
+          <div className={styles.logo}>
+            <CrossLink href="/">
+              <a>
+                <h3 className={styles.logoText}>Vivid Hacks</h3>
               </a>
-            </div>
-            <ul className={this.menu} style={this.menuStyling}>
-              <li className={styles.link}>
-                <a
-                  href="https://vivd.tk/discord"
-                  className={styles.alink}
-                  target="_blank"
-                >
-                  {" "}
-                  DISCORD
-                </a>
-              </li>
-              <li className={styles.link}>
-                <Link href="/" smooth="true" duration="500">
-                  {" "}
-                  TEAM
-                </Link>
-              </li>
-              <li className={styles.link}>
-                <Link
-                  to="timeline"
-                  smooth="true"
-                  duration="500"
-                  className={styles.alink}
-                >
-                  {" "}
-                  TIMELINE
-                </Link>
-              </li>
-              <li className={styles.link}>
-                <Link
-                  to="about"
-                  smooth="true"
-                  duration="500"
-                  className={styles.alink}
-                >
-                  {" "}
-                  ABOUT
-                </Link>
-              </li>
-              <li className={styles.link}>
-                <Link
-                  to="faqs"
-                  smooth="true"
-                  duration="500"
-                  className={styles.alink}
-                >
-                  {" "}
-                  FAQs
-                </Link>
-              </li>
-              <li className={styles.link}>
-                <a
-                  href="https://vividhacks.devpost.com/"
-                  target="_blank"
-                  className={styles.alink}
-                >
-                  {" "}
-                  REGISTER
-                </a>
-              </li>
-            </ul>
+            </CrossLink>
+            <CrossLink href="/">
+              <a>
+                <img
+                  src="logo.svg"
+                  alt="logo.svg"
+                  style={{ width: "5rem" }}
+                />
+              </a>
+            </CrossLink>
+            <a
+              onClick={this.toggleMobileNavbar}
+              className={styles.menuIcon}
+            >
+              <hr className={lineLeft}></hr>
+              <hr className={lineMiddle}></hr>
+              <hr className={lineRight}></hr>
+            </a>
           </div>
+          <ul className={this.menu} style={this.menuStyling}
+            onClick={this.props.callback}
+          >
+            <li className={styles.link}>
+              <a
+                className={styles.alink}
+                target="_blank"
+              >
+                {" "}
+                DISCORD
+              </a>
+            </li>
+            <li className={styles.link}>
+              <Link href="/" smooth="true" duration="500">
+                {" "}
+                TEAM
+              </Link>
+            </li>
+            <li className={styles.link}>
+              <Link
+                to="timeline"
+                smooth="true"
+                duration="500"
+                className={styles.alink}
+              >
+                {" "}
+                TIMELINE
+              </Link>
+            </li>
+            <li className={styles.link}>
+              <Link
+                to="about"
+                smooth="true"
+                duration="500"
+                className={styles.alink}
+              >
+                {" "}
+                ABOUT
+              </Link>
+            </li>
+            <li className={styles.link}>
+              <Link
+                to="faqs"
+                smooth="true"
+                duration="500"
+                className={styles.alink}
+              >
+                {" "}
+                FAQs
+              </Link>
+            </li>
+            <li className={styles.link}>
+              <a
+                href="https://vividhacks.devpost.com/"
+                target="_blank"
+                className={styles.alink}
+              >
+                {" "}
+                REGISTER
+              </a>
+            </li>
+          </ul>
+        </div>
       );
     } else {
       return (
         <div>
-            <div className={styles.logo}>
-              <CrossLink href="/">
-                <a>
-                  <h3 className={styles.logoText}>Vivid Hacks</h3>
-                </a>
-              </CrossLink>
-              <CrossLink href="/">
-                <a>
-                  <img
-                    src="logo.svg"
-                    alt="logo.svg"
-                    style={{ width: "5rem" }}
-                  />
-                </a>
-              </CrossLink>
-              <a
-                onClick={this.toggleMobileNavbar}
-                className={styles.menuIcon}
-              >
-                <hr className={lineLeft}></hr>
-                <hr className={lineMiddle}></hr>
-                <hr className={lineRight}></hr>
+          <div className={styles.logo}>
+            <CrossLink href="/">
+              <a>
+                <h3 className={styles.logoText}>Vivid Hacks</h3>
               </a>
-            </div>
-            <ul className={this.menu} style={this.menuStyling}>
-              <li className={styles.link}>
-                <a
-                  href="https://vivd.tk/discord"
-                  className={styles.alink}
-                  target="_blank"
-                >
-                  DISCORD
-                </a>
-              </li>
-              <li className={styles.link}>
-                <CrossLink href="/#team">
-                  <a> TEAM</a>
-                </CrossLink>
-              </li>
-              <li className={styles.link}>
-                <CrossLink href="/#timeline">
-                  <a className={styles.alink}> TIMELINE</a>
-                </CrossLink>
-              </li>
-              <li className={styles.link}>
-                <CrossLink href="/#about">
-                  <a className={styles.alink}> ABOUT</a>
-                </CrossLink>
-              </li>
-              <li className={styles.link}>
-                <CrossLink href="/#faqs">
-                  <a className={styles.alink}> FAQs</a>
-                </CrossLink>
-              </li>
-              <li className={styles.link}>
-                <a
-                  href="https://vividhacks.devpost.com/"
-                  target="_blank"
-                  className={styles.alink}
-                >
-                  {" "}
-                  REGISTER
-                </a>
-              </li>
-            </ul>
+            </CrossLink>
+            <CrossLink href="/">
+              <a>
+                <img
+                  src="logo.svg"
+                  alt="logo.svg"
+                  style={{ width: "5rem" }}
+                />
+              </a>
+            </CrossLink>
+            <a
+              onClick={this.toggleMobileNavbar}
+              className={styles.menuIcon}
+            >
+              <hr className={lineLeft}></hr>
+              <hr className={lineMiddle}></hr>
+              <hr className={lineRight}></hr>
+            </a>
           </div>
+          <ul className={this.menu} style={this.menuStyling} onClick={this.props.callback}>
+            <li className={styles.link}>
+              <a
+                className={styles.alink}
+                target="_blank"
+              >
+                DISCORD
+              </a>
+            </li>
+            <li className={styles.link}>
+              <CrossLink href="/#team">
+                <a> TEAM</a>
+              </CrossLink>
+            </li>
+            <li className={styles.link}>
+              <CrossLink href="/#timeline">
+                <a className={styles.alink}> TIMELINE</a>
+              </CrossLink>
+            </li>
+            <li className={styles.link}>
+              <CrossLink href="/#about">
+                <a className={styles.alink}> ABOUT</a>
+              </CrossLink>
+            </li>
+            <li className={styles.link}>
+              <CrossLink href="/#faqs">
+                <a className={styles.alink}> FAQs</a>
+              </CrossLink>
+            </li>
+            <li className={styles.link}>
+              <a
+                href="https://vividhacks.devpost.com/"
+                target="_blank"
+                className={styles.alink}
+              >
+                {" "}
+                REGISTER
+              </a>
+            </li>
+          </ul>
+        </div>
       );
     }
   }
