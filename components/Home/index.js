0b91baef67a8f570d76ea5928fbe15f9e2712239
Manish674 <manishchoudhary4905@gmail.com>
2021-11-30 23:42:25 +0530

    Completed Landing page.

--- components/Home/index.js
+++ components/Home/index.js
@@ -2,16 +2,42 @@ import styles from "./home.module.css";
 import Countdown from "../Countdown";
 import logo from "../../public/logo.png";
 import scroll from "../../public/scroll.svg";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
+  const register = () => {
+    window.open("https://vividhacks.devpost.com/");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h1 className={styles.heading}>
           Hacking for <br />
-          Everyone
+          <div className={styles.typewriterContainer}>
+            <Typewriter
+              className={styles.typewriter}
+              options={{
+                strings: [
+                  "EVERYONE",
+                  "HUSTLERS",
+                  "LEARNERS",
+                  "DREAMERS",
+                  "BEGINNERS",
+                  "EXPERTS",
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: "natural",
+              }}
+            />
+          </div>
         </h1>
-        <button className={styles.button}>Register Now</button>
+        <div className={styles.animate}>
+          <button onClick={register} className={styles.offset}>
+            <span className={styles.buttonSpan}>Register Now</span>
+          </button>
+        </div>{" "}
         <div className={styles.date}>November 19, 2021, 5PM EST</div>
         <div className={styles.text}>
           A 65-Hour Student Lead Hackathon <br /> Designed For{" "}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Navbar from "../components/Navbar";
 import Home from "../components/Home";
+import About from "../components/About";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function index() {
+export default function Index() {
+  const { data: session } = useSession();
+
   return (
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   );
 }
